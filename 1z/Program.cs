c76cacf22a9b92e4ce871ec91cddfd3e53c091f7
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-16 00:15:00 +0300

    1z

---
+++ 1z/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+Console.Write("enter storony 1: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("enter  storony 2: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("enter  storony 3 : ");
+int c = int.Parse(Console.ReadLine() ?? "0");
+
+if (a+b>c && b+c>a && c+a>b)
+{
+Console.Write(" it is triangle: ");
+}
+else
+Console.Write("no triangle: ");
