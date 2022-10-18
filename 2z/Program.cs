27575e63ee7a4e3fbb97587285dae5d467fc31b7
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-18 21:38:26 +0300

    2zzz

--- 2z/Program.cs
+++ 2z/Program.cs
@@ -1,37 +1,72 @@
-﻿// Написать программу преобразования десятичного числа в двоичное
+﻿// // Написать программу преобразования десятичного числа в двоичное
 
-void PrintArray(int [] array)
+// void PrintArray(int [] array)
+// {
+//    foreach(int element in array)
+//    {
+//     Console.Write($"{element}");
+//    }
+//    Console.WriteLine();
+// }
+
+// Console.WriteLine ("enter number:");
+// int number=int.Parse(Console.ReadLine() ?? "0");
+// int i=0;
+
+// if (number>0)
+// {
+//    while (number>0)
+//    {
+//     int number2=number%2;
+//     number=number/2;
+//     i++;
+//    }
+//    int [] array=new int [i];
+//   int count=i-1;
+
+//     while (number>0)
+//     {
+//       int number3=number%2;
+//       number=number/2;
+//       array [count]=number3;
+//       count--;
+//     }
+//  Console.Write ($" в двоичной =");
+//  PrintArray(array);
+// }
+// else
+// {
+// Console.Write ($" в двоичной =0");
+// }
+// НЕ РАБОТАЕТ
+
+Console.Write("Введите число: ");
+int chislo = int.Parse(Console.ReadLine() ?? "0");
+double result = 0;
+int i = 0;
+while (chislo != 0)
 {
-   foreach(int element in array)
-   {
-    Console.Write($"{element}");
-   }
-   Console.WriteLine();
-}
-
-Console.WriteLine ("enter number:");
-int number=int.Parse(Console.ReadLine() ?? "0");
-int [] array=new int [number];
-int i=0;
-
-if (number%2==0)
-{
-   while (number>0)
-   {
-    int number2=number%2;
-    number=number/2;
+    result = result + (chislo % 2 * Math.Pow(10, i));
+    chislo = chislo / 2;
     i++;
-   }
 }
-else
-{
-    while (number>0)
-    {
-      int number2=number%2;
-      number=number/2;
-      array [i]=number2;
-      i++;
-    }
-}    
-Console.Write ($" в двоичной =");
-PrintArray(array);
+Console.WriteLine(result);
+
+//2 решение не работает
+
+// Console.Write("Введите число: ");
+// int number= int.Parse(Console.ReadLine() ?? "0");
+// string result=string.Empty;
+// string.output=string.Empty;
+// while (number !=0)
+// {
+//   result=result+number%2;
+//   number=number/2;
+// }
+
+// for (int i=result.Lenght-1;i>=0;i--)
+// {
+//    output +=result[i];
+
+// }
+// Console.WriteLine(output);
